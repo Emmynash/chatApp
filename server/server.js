6dde53ad328790614ce34cb62dbadd55fd591114
E Akita <emmy4nash@gmail.com>
2019-02-06 15:32:47 +0100

    connect and disconnect socketio

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html', (err) => {
-//         if (err) {
-//             return console.log(err);
-//         }
-//     })
-// })
-app.listen(port, (err) => {
+io.on('connection', (socket) => {
+        console.log("New user connected");
+
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        })
+    })
+    // app.get('/', (req, res) => {
+    //     res.sendFile('index.html', (err) => {
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //     })
+    // })
+server.listen(port, (err) => {
     if (err) {
         return console.log(`server unable to start: ${err}`)
     }

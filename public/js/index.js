5b734c2ff5595db02737276ce09ceffa8c868417
E Akita <emmy4nash@gmail.com>
2019-02-09 18:52:47 +0100

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 // const moment = require('moment');
 var socket = io();
+
+function scrollToBottom() {
+    var messages = jQuery("#listMessage");
+
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on("connect", function() {
     console.log("connected to the server")
 
@@ -20,6 +38,7 @@ socket.on("serverMessage", function(message) {
 
 
     jQuery("#listMessage").append(html);
+    scrollToBottom();
 })
 
 socket.on("newLocationMessage", function(message) {
@@ -35,6 +54,7 @@ socket.on("newLocationMessage", function(message) {
 
 
     jQuery("#listMessage").append(html);
+    scrollToBottom();
 
 })
 
